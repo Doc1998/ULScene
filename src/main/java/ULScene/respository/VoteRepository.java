8cfc159682ee8e287d0dfe129cee0e62f2f96b54
Doc1998 <docmp98@gmail.com>
2020-11-12 22:36:23 +0000

    Ready for presentation

---
+++ src/main/java/ULScene/respository/VoteRepository.java
@@ -0,0 +1,9 @@
+package ULScene.respository;
+
+import ULScene.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Long> {
+}
